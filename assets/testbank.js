015183ec001d356e8c4df9a57bc8b971d89ec6e5
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-01-24 16:18:24 -0800

    Testing the testbank.js

---
+++ assets/testbank.js
@@ -0,0 +1,28 @@
+let testBank = [
+    {
+        "problemStatement": "What is the HTML tag under which one can write the JavaScript code?",
+        "code": [],
+        "choices": [
+            "<javascript>",
+            "<scripted>",
+            "<script>",
+            "<js>"
+        ],
+        "answer": 2,
+        "solved": false
+    },
+    {
+        "problemStatement": "Choose the correct JavaScript syntax to change the content of the following HTML code.",
+        "code": [
+            "<p id=\"geek\">GeeksforGeeks</p>"
+        ],
+        "choices": [
+            "<javascript>",
+            "<scripted>",
+            "<script>",
+            "<js>"
+        ],
+        "answer": 2,
+        "solved": false
+    },
+];
