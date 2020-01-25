367435dca1bb6dee47fc6865804050517df24a1b
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-01-24 23:48:32 -0800

    Working Prototype for single problem (randomly chosen from test bank)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,37 @@
 // Constants
 const DIV_CLASS = "col-md-8 col-sm-12 mx-auto";
 const CARD_CLASS = "card text-left p-auto";
+const CORRECT_BUTTON_CLASS = "btn btn-success btn-md btn-block text-left";
+const WRONG_BUTTON_CLASS = "btn btn-danger btn-md btn-block text-left";
+
+// How many problem in a quiz?
+const NUM_PROBLEM = 10;
+
+/**************************************************** Main Function ****************************************************/
+let score = {
+  attempt: 0,
+  solved: 0,
+  solvedLast: false
+}
+
+// Shuffle the Test Bank
+shuffleTestBank();
+
+// Begin Quiz
+takeQuiz();
+
+function takeQuiz() {
+  if(score.attempt < NUM_PROBLEM) {
+    buildProblem(score.attempt);
+  }
+  else {
+    finishQuiz();
+  }
+}
+
+function finishQuiz() {
+  displayResult();
+}
 
 /**************************************************** Problem Building ****************************************************/
 
@@ -72,6 +103,108 @@ function buildProblem(problemIndex) {
 }
 
 //removeElement("problem");
+/**************************************************** Button Functions ****************************************************/
+function displayExplanation() {
+  // create div
+  let div = makeClassedElement("div", DIV_CLASS);
+  div.setAttribute("id", "explanation");
+
+  // append div
+  document.getElementById("explanation-wrapper").appendChild(div);
+
+  // CORRECT OR WRONG
+  let correctOrWrong = document.createElement("h5");
+  correctOrWrong.innerHTML = score.solvedLast ? "You're Correct!" : "Wrong."
+  document.getElementById("explanation").appendChild(correctOrWrong);
+
+  // create explanation
+  let explanation = document.createElement("h5");
+  explanation.innerHTML = testBank[score.attempt].explanation;
+
+  // append explanation
+  document.getElementById("explanation").appendChild(explanation);
+
+  // SHOW BUTTONS
+}
+
+
+/**************************************************** Button Functions ****************************************************/
+function buttonA() {
+  let btnA = document.getElementById("choices").children[0];
+  let correctAnswer = testBank[score.attempt].answer
+  // CORRECT
+  if(correctAnswer === 0) {
+    btnA.setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solved++;
+    score.solvedLast = true;
+  }
+  // WRONG
+  else {
+    btnA.setAttribute("class", WRONG_BUTTON_CLASS);
+    document.getElementById("choices").children[correctAnswer].setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solvedLast = false;
+  }
+
+  displayExplanation();
+}
+
+function buttonB() {
+  let btnB = document.getElementById("choices").children[1];
+  let correctAnswer = testBank[score.attempt].answer
+  // CORRECT
+  if(correctAnswer === 1) {
+    btnB.setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solved++;
+    score.solvedLast = true;
+  }
+  // WRONG
+  else {
+    btnB.setAttribute("class", WRONG_BUTTON_CLASS);
+    document.getElementById("choices").children[correctAnswer].setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solvedLast = false;
+  }
+
+  displayExplanation();
+}
+
+function buttonC() {
+  let btnC = document.getElementById("choices").children[2];
+  let correctAnswer = testBank[score.attempt].answer
+  // CORRECT
+  if(correctAnswer === 2) {
+    btnC.setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solved++;
+    score.solvedLast = true;
+  }
+  // WRONG
+  else {
+    btnC.setAttribute("class", WRONG_BUTTON_CLASS);
+    document.getElementById("choices").children[correctAnswer].setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solvedLast = false;
+  }
+
+  displayExplanation();
+}
+
+function buttonD() {
+  let btnD = document.getElementById("choices").children[3];
+  let correctAnswer = testBank[score.attempt].answer
+  // CORRECT
+  if(correctAnswer === 3) {
+    btnD.setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solved++;
+    score.solvedLast = true;
+  }
+  // WRONG
+  else {
+    btnD.setAttribute("class", WRONG_BUTTON_CLASS);
+    document.getElementById("choices").children[correctAnswer].setAttribute("class", CORRECT_BUTTON_CLASS);
+    score.solvedLast = false;
+  }
+
+  displayExplanation();
+}
+
 
 /**************************************************** Utility Functions ****************************************************/
 
@@ -109,8 +242,18 @@ function removeElement(elementId) {
   element.parentNode.removeChild(element);
 }
 
+function shuffleTestBank() {
+  let current = testBank.length;
+  let temp, random;
+
+  while( 0 != current) {
+    random = Math.floor(Math.random() * current);
+    current -= 1;
+
+    temp = testBank[current];
+    testBank[current] = testBank[random];
+    testBank[random] = temp;
+  }
+}
 
 
-
-/**************************************************** Function Testing ****************************************************/
-buildProblem(1);
