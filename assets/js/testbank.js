4ff3f56e8cfa4452fdeef2a98368860c9e6d540d
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-01-25 00:15:32 -0800

    updated test bank (now has 30 problems)

--- assets/js/testbank.js
+++ assets/js/testbank.js
@@ -220,40 +220,179 @@ let testBank = [
         "explanation": `In JavaScript, functions are called directly like x().`
     },
     {
-        "problemStatement": `Which of the following is not a reserved word in JavaScript?`,
+        "problemStatement": `How to write an ‘if’ statement for executing some code.
+        If “i” is NOT equal to 5?`,
         "code": [],
         "choices": [
-            `interface`,
-            `throws`,
-            `program`,
-            `short`
+            `if(i<>5)`,
+            `if i<>5`,
+            `if(i!=5)`,
+            `if i!=5`
         ],
         "answer": 2,
-        "explanation": `In JavaScript, interface, throws and short are reserved keywords.`
+        "explanation": `JavaScript do not accept <> operator as not equal to.`
     },
     {
-        "problemStatement": `Which of the following is not a reserved word in JavaScript?`,
+        "problemStatement": `What is the correct syntax for adding comments in JavaScript?`,
         "code": [],
         "choices": [
-            `interface`,
-            `throws`,
-            `program`,
-            `short`
+            `<!–This is a comment–&gt;`,
+            `//This is a comment`,
+            `–This is a comment`,
+            `**This is a comment**`
         ],
-        "answer": 2,
-        "explanation": `In JavaScript, interface, throws and short are reserved keywords.`
+        "answer": 1,
+        "explanation": `Correct Syntax for comments in JavaScript is //comment.`
     },
     {
-        "problemStatement": `Which of the following is not a reserved word in JavaScript?`,
+        "problemStatement": `How to insert a multi-line comment in JavaScript?`,
         "code": [],
         "choices": [
-            `interface`,
-            `throws`,
-            `program`,
-            `short`
+            `<!–This is comment line 1
+            This is comment line 2–&gt;`,
+            `//This is comment line 1
+            This is comment line 2//`,
+            `/*This is comment line 1
+            This is comment line 2*/`,
+            `**This is comment line 1
+            This is comment line 2**`
         ],
         "answer": 2,
-        "explanation": `In JavaScript, interface, throws and short are reserved keywords.`
+        "explanation": `Correct Syntax for multi-line comments in JavaScript is /*comment*/.`
+    },
+    {
+        "problemStatement": `What is the JavaScript syntax for printing values in Console?`,
+        "code": [],
+        "choices": [
+            `print(5)`,
+            `console.log(5);`,
+            `console.print(5);`,
+            `print.console(5);`
+        ],
+        "answer": 1,
+        "explanation": `The action which is built into the console object is the .log() method. Whenever we write console.log() in the JavaScript code, what we put inside the parentheses will get printed, or logged, to the console.`
+    },
+    {
+        "problemStatement": `How to initialize an array in JavaScript?`,
+        "code": [],
+        "choices": [
+            `var Geeks= “Geek1”, “Geek2”, “Geek3”`,
+            `var Geeks=(1:Geek1, 2:Geek2, 3:Geek3)`,
+            `var Geeks=(1=Geek1, 2=Geek2, 3=Geek3)`,
+            `var Geeks=[“Geek1”, “Geek2”, “Geek3”]`
+        ],
+        "answer": 3,
+        "explanation": `In JavaScript, functions are called directly like x().`
+    },
+    {
+        "problemStatement": `What will be the output of the following code?`,
+        "code": [
+            `<script>`,
+            `document.write(typeof(24.49));`,
+            `</script>`
+        ],
+        "choices": [
+            `float`,
+            `number`,
+            `integer`,
+            `double`
+        ],
+        "answer": 1,
+        "explanation": `There are seven fundamental data types in JavaScript. They are number, string, boolean, null, undefined, symbol and object. We do not have data types like float, integer and double in JavaScript.`
+    },
+    {
+        "problemStatement": `What will be the command to print the number of characters in the string “GeeksforGeeks”?`,
+        "code": [],
+        "choices": [
+            `document.write(“GeeksforGeeks”.len);`,
+            `document.write(sizeof(“GeeksforGeeks”));`,
+            `document.write(“GeeksforGeeks”.length);`,
+            `document.write(lenof(“GeeksforGeeks”));`
+        ],
+        "answer": 2,
+        "explanation": `The .length property of JavaScript is used to evaluate the number of characters in any string.`
+    },
+    {
+        "problemStatement": `What is the method in JavaScript used to remove the whitespace at the beginning and end of any string ?`,
+        "code": [],
+        "choices": [
+            `strip()`,
+            `trim()`,
+            `stripped()`,
+            `trimmed()`
+        ],
+        "answer": 1,
+        "explanation": `The trim() method in JavaScript is used to remove the whitespaces at the beginning and end of the string.`
+    },
+    {
+        "problemStatement": `In JavaScript, we do not have datatypes like integer and float. What is the function that can be used to check if the number is an integer or not?`,
+        "code": [],
+        "choices": [
+            `Integer(value)`,
+            `ifInteger(value)`,
+            `isInteger(value)`,
+            `ifinteger(value)`
+        ],
+        "answer": 2,
+        "explanation": `isInteger() function is used to check whether a number is integer or not. The function is used as: document.write(number.isInteger(2018)) will result as true.`
+    },
+    {
+        "problemStatement": `Which of the following is an advantage of using JavaScript?`,
+        "code": [],
+        "choices": [
+            `Increased interactivity.`,
+            `Less server interaction.`,
+            `Immediate feedback from the users.`,
+            `All of the above.`
+        ],
+        "answer": 3,
+        "explanation": `JavaScript ensures increased interactivity, less server interation and immediate feedback from the users.`
+    },
+    {
+        "problemStatement": `Which function of an Array object calls a function for each element in the array?`,
+        "code": [],
+        "choices": [
+            `forEach()`,
+            `every()`,
+            `forEvery()`,
+            `each()`
+        ],
+        "answer": 0,
+        "explanation": `forEach() – Calls a function for each element in the array.`
+    },
+    {
+        "problemStatement": `JavaScript is a ________ Side Scripting Language.`,
+        "code": [],
+        "choices": [
+            `Server`,
+            `Browser`,
+            `ISP`,
+            `None of the above`
+        ],
+        "answer": 1,
+        "explanation": `JavaScript is a Browser Side Scripting Language. ASP, PHP, Perl are Server Side Scripting Language.`
+    },
+    {
+        "problemStatement": `JavaScript is ________ language.`,
+        "code": [],
+        "choices": [
+            `an interpreted`,
+            `a compiled`
+        ],
+        "answer": 0,
+        "explanation": `JavaScript is an interpreted language, not a compiled language. C++ or Java codes needs to be compiled before it is run. The source code is passed through a program called a compiler, which translates it into bytecode that the machine understands and can execute. There is no such need in case of JavaScript.`
+    },
+    {
+        "problemStatement": `Which was the first browser to support JavaScript?`,
+        "code": [],
+        "choices": [
+            `Mozilla Firefox`,
+            `Netscape`,
+            `Google Chrome`,
+            `IE`
+        ],
+        "answer": 1,
+        "explanation": `Netscape was the first web browser to support JavaScript.`
     },
 
 ];
